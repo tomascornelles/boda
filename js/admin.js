86777c52f6ab11e7ec2294f889165c7e2f1ab66d
menos5 <menos5@gmail.com>
2016-04-28 16:08:52 +0200

    confirmados

--- js/admin.js
+++ js/admin.js
@@ -1,13 +1,31 @@
 /* global $ google page Firebase localStorage */
 
-new Firebase('https://boda201610.firebaseio.com/')
+var resultados = $('.resultados')
+
+function consultaConfirmados (e) {
+  e.preventDefault()
+  new Firebase('https://boda201610.firebaseio.com/')
   .orderByChild('confirmar')
   .equalTo('Confirmado')
   .once('value', function (snap) {
     var res = snap.val()
     // console.log(snap.val())
+    resultados.empty().css({'background': 'url(../img/squares.gif) center center no-repeat', 'height': '100vh'})
+    var tabla = '<table style="width:100%"><thead><tr><th>Nombre</th><th>Adultos</th><th>Niños</th><th>Comida</th></tr></thead><tbody>'
     for (var i in res) {
       var Invitado = res[i]
-      console.log(Invitado.nombre + '\n - adultos: ' + Invitado.adultos + '\n - ninos: ' + Invitado.ninos + '\n - comida: ' + Invitado.comida)
+      var nombre = (Invitado.nombre) ? Invitado.nombre : 'Anonimo'
+      var adultos = (Invitado.adultos) ? Invitado.adultos : '0'
+      var ninos = (Invitado.ninos) ? Invitado.ninos : '0'
+      var comida = (Invitado.comida) ? Invitado.comida : ''
+      tabla += '<tr><th>' + nombre + '</th><td>' + adultos + '</td><td>' + ninos + '</td><td>' + comida + '</td></tr>'
     }
+    tabla += '</tbody></table>'
+    
+    resultados.css({'background': '', 'height': ''}).html(tabla)
   })
+}
+
+$(function () {
+  $('.confirmados').on('click', consultaConfirmados)
+})

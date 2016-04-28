b773d3f1b3f930b6537370edbf8d47db92dda3ae
menos5 <menos5@gmail.com>
2016-04-28 16:35:04 +0200

    Mensajes

--- js/admin.js
+++ js/admin.js
@@ -5,27 +5,58 @@ var resultados = $('.resultados')
 function consultaConfirmados (e) {
   e.preventDefault()
   new Firebase('https://boda201610.firebaseio.com/')
-  .orderByChild('confirmar')
-  .equalTo('Confirmado')
-  .once('value', function (snap) {
-    var res = snap.val()
-    // console.log(snap.val())
-    resultados.empty().css({'background': 'url(../img/squares.gif) center center no-repeat', 'height': '100vh'})
-    var tabla = '<table style="width:100%"><thead><tr><th>Nombre</th><th>Adultos</th><th>Niños</th><th>Comida</th></tr></thead><tbody>'
-    for (var i in res) {
-      var Invitado = res[i]
-      var nombre = (Invitado.nombre) ? Invitado.nombre : 'Anonimo'
-      var adultos = (Invitado.adultos) ? Invitado.adultos : '0'
-      var ninos = (Invitado.ninos) ? Invitado.ninos : '0'
-      var comida = (Invitado.comida) ? Invitado.comida : ''
-      tabla += '<tr><th>' + nombre + '</th><td>' + adultos + '</td><td>' + ninos + '</td><td>' + comida + '</td></tr>'
-    }
-    tabla += '</tbody></table>'
-    
-    resultados.css({'background': '', 'height': ''}).html(tabla)
-  })
+    .orderByChild('confirmar')
+    .equalTo('Confirmado')
+    .once('value', function (snap) {
+      var res = snap.val()
+      // console.log(snap.val())
+      resultados.empty().css({'background': 'url(../img/squares.gif) center center no-repeat', 'height': '100vh'})
+      var tabla = '<table style="width:100%"><thead><tr><th>Nombre</th><th>Adultos</th><th>Niños</th><th>Comida</th></tr></thead><tbody>'
+      var totales = {
+        adultos: 0,
+        ninos: 0
+      }
+      for (var i in res) {
+        var Invitado = res[i]
+        var nombre = (Invitado.nombre) ? Invitado.nombre : 'Anonimo'
+        var adultos = (Invitado.adultos) ? Invitado.adultos : 0
+        totales.adultos += parseInt(adultos)
+        var ninos = (Invitado.ninos) ? Invitado.ninos : 0
+        totales.ninos += parseInt(ninos)
+        var comida = (Invitado.comida) ? Invitado.comida : ''
+        tabla += '<tr><th>' + nombre + '</th><td>' + adultos + '</td><td>' + ninos + '</td><td>' + comida + '</td></tr>'
+      }
+      tabla += '<tr><th>Total</th><th>' + totales.adultos + '</th><th>' + totales.ninos + '</th><td></td></tr>'
+      tabla += '</tbody></table>'
+
+      resultados.css({'background': '', 'height': ''}).html(tabla)
+    })
+}
+
+function consultaMensajes (e) {
+  e.preventDefault()
+  new Firebase('https://boda201610.firebaseio.com/')
+    .orderByChild('mensaje')
+    .once('value', function (snap) {
+      var res = snap.val()
+      // console.log(snap.val())
+      resultados.empty().css({'background': 'url(../img/squares.gif) center center no-repeat', 'height': '100vh'})
+      var tabla = '<table style="width:100%"><thead><tr><th>Nombre</th><th>Mensaje</th></tr></thead><tbody>'
+      for (var i in res) {
+        var Invitado = res[i]
+        if (Invitado.mensaje) {
+          var nombre = (Invitado.nombre) ? Invitado.nombre : 'Anonimo'
+          var mensaje = Invitado.mensaje.replace('\n','<br>')
+          tabla += '<tr><th>' + nombre + '</th><td>' + mensaje + '</td></tr>'
+        }
+      }
+      tabla += '</tbody></table>'
+
+      resultados.css({'background': '', 'height': ''}).html(tabla)
+    })
 }
 
 $(function () {
   $('.confirmados').on('click', consultaConfirmados)
+  $('.mensajes').on('click', consultaMensajes)
 })

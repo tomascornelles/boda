8e73357427205610e237f36727564658524ff705
menos5 <menos5@gmail.com>
2016-04-28 17:20:22 +0200

    Charas

--- js/admin.js
+++ js/admin.js
@@ -46,7 +46,7 @@ function consultaMensajes (e) {
         var Invitado = res[i]
         if (Invitado.mensaje) {
           var nombre = (Invitado.nombre) ? Invitado.nombre : 'Anonimo'
-          var mensaje = Invitado.mensaje.replace('\n','<br>')
+          var mensaje = Invitado.mensaje.replace('\n', '<br>')
           tabla += '<tr><th>' + nombre + '</th><td>' + mensaje + '</td></tr>'
         }
       }
@@ -55,8 +55,62 @@ function consultaMensajes (e) {
       resultados.css({'background': '', 'height': ''}).html(tabla)
     })
 }
+function consultaPixelitos (e) {
+  e.preventDefault()
+  new Firebase('https://boda201610.firebaseio.com/')
+    .orderByChild('chara')
+    .once('value', function (snap) {
+      var res = snap.val()
+      // console.log(snap.val())
+      resultados.empty().css({'background': 'url(../img/squares.gif) center center no-repeat', 'height': '100vh'})
+      var tabla = '<table style="width:100%"><thead><tr><th>Nombre</th><th style="width:64px">Pixelito</th></tr></thead><tbody>'
+      for (var i in res) {
+        var Invitado = res[i]
+        if (Invitado.chara) {
+          var nombre = (Invitado.nombre) ? Invitado.nombre : 'Anonimo'
+          var chara = CharaMaker(Invitado.chara)
+          tabla += '<tr><th>' + nombre + '</th><td><svg preserveAspectRatio="xMinYMin meet" viewBox="0 0 17 17">' + chara + '</svg></td></tr>'
+        }
+      }
+      tabla += '</tbody></table>'
+
+      resultados.css({'background': '', 'height': ''}).html(tabla)
+    })
+}
+/* Chara */
+function CharaMaker (chara) {
+  var miSVG = $('#my-svg').clone()
+  miSVG.find('.svg_body').css({'fill': chara.svg_body})
+  miSVG.find('.svg_hair').css({'fill': chara.svg_hair}).hide()
+  if (chara.hairType === 'corto') {
+    miSVG.find('.svg_hair_corto').show()
+  } else {
+    miSVG.find('.svg_hair_largo').show()
+  }
+  miSVG.find('.svg_beard').css({'fill': chara.svg_beard})
+  miSVG.find('.svg_mustache').css({'fill': chara.svg_mustache})
+  miSVG.find('.svg_eyes').css({'fill': chara.svg_eyes})
+  miSVG.find('.svg_shirt').css({'fill': chara.svg_shirt}).hide()
+  miSVG.find('.svg_shirt_corto').show()
+  miSVG.find('.svg_shirt_chaleco').css({'fill': chara.svg_shirt_chaleco})
+  miSVG.find('.svg_shirt_tirantes').css({'fill': chara.svg_shirt_tirantes})
+  miSVG.find('.svg_shirt_rayas').css({'fill': chara.svg_shirt_rayas})
+  miSVG.find('.svg_shirt_flash').css({'fill': chara.svg_shirt_flash})
+  miSVG.find('.svg_pants').css({'fill': chara.svg_pants}).hide()
+  if (chara.pantType === 'falda') {
+    miSVG.find('.svg_pants_falda').show()
+  } else if (chara.pantType === 'corto') {
+    miSVG.find('.svg_pants_corto').show()
+  } else {
+    miSVG.find('.svg_pants_largo').show()
+  }
+  miSVG.find('.svg_shoes').css({'fill': chara.svg_shoes})
+  
+  return miSVG.html()
+}
 
 $(function () {
   $('.confirmados').on('click', consultaConfirmados)
   $('.mensajes').on('click', consultaMensajes)
+  $('.pixelitos').on('click', consultaPixelitos)
 })
